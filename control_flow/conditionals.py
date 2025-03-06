0b240c5e3fc6a942420217b172abc2c3a86140f7
ayorick23 <mayorickhenry@gmail.com>
2025-03-05 18:10:20 -0600

    Se agrego la carpeta de flujos de control y un primer archivo

---
+++ control_flow/conditionals.py
@@ -0,0 +1,14 @@
+#Uso de if, elif y else
+x = 10
+if x > 10:
+    print("Mayor que 10")
+elif x == 10:
+    print("Igual a 10")
+else:
+    print("Menor que 10")
+
+#Condiciones anidadas
+y = 5
+if x > 5:
+    if y < 10:
+        print("x es mayor que 5 e y es menor que 10")
